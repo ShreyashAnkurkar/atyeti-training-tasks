d8b4645f9ecc0031eaaa6de45992c7b1203c8e12
Shreyash Ankurkar <144425421+ShreyashAnkurkar@users.noreply.github.com>
2026-06-25 16:14:55 +0530

    Task Complete
    - Added addEmployee()
    - Added updateEmployee()
    - Added deleteEmployee()
    - Added searchEmployeeById()
    - Added displayAllEmployees()

---
+++ Shreyash/Task1-EmployeeMgmtSystem/src/main/java/com/atyeti/service/EmployeeServiceImpl.java
@@ -0,0 +1,47 @@
+package com.atyeti.service;
+
+import com.atyeti.model.Employee;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class EmployeeServiceImpl implements IEmployeeService{
+
+    private  final Set<Employee> empData=new LinkedHashSet<>();
+    @Override
+    public boolean addEmployee(Employee e) {
+        return empData.add(e);
+    }
+
+    @Override
+    public boolean updateEmployee(int id,Employee e) {
+        if(searchEmployeeById(id)==null) return false;
+
+        Employee emp=searchEmployeeById(id);
+        emp.setName(e.getName());
+        emp.setId(emp.getId());
+        emp.setAddress(emp.getAddress());
+        emp.setDept(emp.getDept());
+
+        return true;
+
+    }
+
+    @Override
+    public boolean deleteEmployee(int id) {
+        Employee e=searchEmployeeById(id);
+        return empData.remove(e);
+    }
+
+    @Override
+    public Employee searchEmployeeById(int id) {
+        return empData.stream().filter(e->e.getId()==id).findFirst().orElse(null);
+    }
+
+    @Override
+    public List<Employee> displayAllEmployees() {
+        return new ArrayList<>(empData);
+    }
+}
